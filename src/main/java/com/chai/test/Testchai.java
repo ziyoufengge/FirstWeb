8c910ea42f6003d02af73039a46649b69e194f36
ziyoufengge <1254797146@qq.com>
2018-01-04 13:06:20 +0800

    hello world!

---
+++ src/main/java/com/chai/test/Testchai.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.chai.test;
+
+/**
+ * @author chaizheng
+ *
+ */
+public class Testchai {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world!");
+
+	}
+
+}
