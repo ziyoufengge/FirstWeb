38307293a1ff6715e419719f4c48ef60702a564a
ziyoufengge <1254797146@qq.com>
2018-01-05 10:58:03 +0800

    测试

---
+++ src/main/java/com/chai/pojo/TestChai.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.chai.pojo;
+
+/**
+ * @author chaizheng
+ * 描述：test类
+ * @date 2018年1月5日
+ */
+public class TestChai {
+
+	/**
+	 * 描述：
+	 * @date  2018年1月5日
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
