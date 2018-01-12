2215fe0533941721a9e73da7667faa1d30321219
ziyoufengge <1254797146@qq.com>
2018-01-12 12:23:21 +0800

    提交到sit上

---
+++ src/main/java/com/chai/test/TestChai3.java
@@ -0,0 +1,24 @@
+/**
+ * @author chaizheng
+ * 描述：
+ * @date 2018年1月12日
+ */
+package com.chai.test;
+
+/**
+ * @author chaizheng
+ * 描述：
+ * @date 2018年1月12日
+ */
+public class TestChai3 {
+
+	/**
+	 * 描述：
+	 * @date  2018年1月12日
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
